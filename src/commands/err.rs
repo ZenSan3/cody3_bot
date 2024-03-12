ecef08c3b8bcb1da8e756be07a1c4951348e1d33
cristian-zeni <cristian.zeni04@gmail.com>
2024-03-12 16:11:34 +0100

    ended error handling for opening file

--- src/commands/err.rs
+++ src/commands/err.rs
@@ -1,16 +1,16 @@
-use std::fs::File;
-use std::io::prelude::*;
-use serde_json::Value;
 use serenity::model::channel::Message;
 use serenity::builder::{CreateEmbed, CreateMessage};
 use serenity::prelude::*;
 
+use super::file::opfile;
+
 pub async fn errore(error:usize, ctx:&Context, msg:&Message)->String{
     
-    let mut file = File::open("./json/errors.json").unwrap();
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
-    let data: Value = serde_json::from_str(&contents).unwrap();
+    let data = match opfile("./json/errors.json".to_owned(), ctx, msg).await {
+        Ok(dati)=> dati,
+        Err(_err)=>return "Errore apertura file".to_owned(),
+    };
+    
 
     let embed = CreateEmbed::new()
         .color(0xff0000)
@@ -32,4 +32,23 @@ pub async fn errore(error:usize, ctx:&Context, msg:&Message)->String{
 
     return " ".to_owned();
 
+}
+
+pub async fn ferror(ctx:&Context, msg:&Message){
+    let embed = CreateEmbed::new()
+        .color(0xff0000)
+        .title("Error")
+        .description("The follow error has occured:")
+        .field(
+            "ListAccessError",
+            "It was not possible to access the list of the requested command @cmd",
+            false
+        );
+
+    let builder = CreateMessage::new()
+        .embed(embed);
+
+    if let Err(why) = msg.channel_id.send_message(&ctx.http, builder).await{
+        println!("Error sending message: {why:?}");
+    }
 }
--- src/commands/file.rs
+++ src/commands/file.rs
@@ -1,5 +1,27 @@
+use serde_json::Value;
+use std::fs::File;
+use std::io::prelude::*;
+use serenity::model::channel::Message;
+use serenity::prelude::*;
 
+use super::err::ferror;
 
-pub async fn opfile(){
+pub async fn opfile(path:String, ctx:&Context, msg:&Message)->Result<Value, String>{
 
+    if let Ok(mut file) = File::open(path){
+        let mut contents = String::new();
+        
+        if let Err(_err) = file.read_to_string(&mut contents){
+            ferror(ctx, msg).await;
+        }
+
+        if let Ok(data) = serde_json::from_str(&contents){
+            return Ok(data);
+        }else{
+            return Err("".to_owned());
+        }
+    }else{
+        return Err("".to_owned());
+    }
 }
+    
